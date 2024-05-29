c86bf073216db28105ffb2ef7127819ccd6aad65
Alice Martynova <alicemartynova94@gmail.com>
2024-05-29 10:54:03 +0300

    initial commit

---
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.banking.banking_microservice_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMicroserviceTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMicroserviceTestTaskApplication.class, args);
+	}
+
+}
---
+++ banking_microservice_test_task/src/test/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banking_microservice_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingMicroserviceTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
