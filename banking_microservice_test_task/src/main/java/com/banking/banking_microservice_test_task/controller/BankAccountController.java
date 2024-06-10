fd081454e764741c48ad638d5c45ad76792a4c08
Alice Martynova <alicemartynova94@gmail.com>
2024-06-10 17:45:53 +0300

    feat: add BankAccountController with GET method to retrieve bank account details, DELETE method to remove an account and, SAVE method to create or update an account, added necessary mapping.

---
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
@@ -0,0 +1,34 @@
+package com.banking.banking_microservice_test_task.controller;
+
+import com.banking.banking_microservice_test_task.entity.BankAccount;
+import com.banking.banking_microservice_test_task.service.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
+public class BankAccountController {
+    @Autowired
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/account/{id}")
+    public BankAccount getAccount(@PathVariable UUID id) {
+        BankAccount bankAccount = bankAccountService.getAccount(id);
+        return bankAccount;
+    }
+
+    @PostMapping("/accounts")
+    public BankAccount addNewAccount(@RequestBody BankAccount bankAccount) {
+        bankAccountService.saveAccount(bankAccount);
+        return bankAccount;
+    }
+
+    @DeleteMapping("/account/{id}")
+    public String deleteAccount(@PathVariable UUID id) {
+        bankAccountService.deleteAccount(id);
+        return "Account " + id + " was deleted.";
+    }
+}
+
