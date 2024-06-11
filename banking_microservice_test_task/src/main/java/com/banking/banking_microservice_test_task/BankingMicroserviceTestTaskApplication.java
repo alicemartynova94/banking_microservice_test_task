110acee5b5d657f63701902921a3ddb1f7806622
Alice Martynova <alicemartynova94@gmail.com>
2024-06-11 09:16:33 +0300

    feat: add logging

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplication.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplication.java
@@ -1,13 +1,18 @@
 package com.banking.banking_microservice_test_task;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankingMicroserviceTestTaskApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BankingMicroserviceTestTaskApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankingMicroserviceTestTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        logger.info("Starting the application");
+        SpringApplication.run(BankingMicroserviceTestTaskApplication.class, args);
+        logger.info("Application started successfully");
+    }
 
 }
