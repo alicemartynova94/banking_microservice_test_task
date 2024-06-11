d4cf6d1ba08a424b90033d6ed3e1fdcfc525bf3f
Alice Martynova <alicemartynova94@gmail.com>
2024-06-11 09:11:27 +0300

    style: delete unused imports

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
@@ -1,14 +1,11 @@
 package com.banking.banking_microservice_test_task.controller;
 
 import com.banking.banking_microservice_test_task.entity.BankAccount;
-import com.banking.banking_microservice_test_task.exception_handling.BankAccountIncorrectData;
 import com.banking.banking_microservice_test_task.exception_handling.NoSuchBankAccountException;
 import com.banking.banking_microservice_test_task.service.BankAccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
