82cbd92d9fcf6d8b68f4a7a76bd7e5e80c6d9dfc
Alice Martynova <alicemartynova94@gmail.com>
2024-06-17 17:34:48 +0300

    refactor: change logging to slf4j annotation, move exception handling to getAccount and deleteAccount methods

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/BankAccountServiceImpl.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/BankAccountServiceImpl.java
@@ -2,44 +2,44 @@ package com.banking.banking_microservice_test_task.service;
 
 import com.banking.banking_microservice_test_task.dao.BankAccountRepository;
 import com.banking.banking_microservice_test_task.entity.BankAccount;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.banking.banking_microservice_test_task.exception_handling.NoSuchBankAccountException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BankAccountServiceImpl implements BankAccountService {
-    private static final Logger logger = LoggerFactory.getLogger(BankAccountServiceImpl.class);
+
     @Autowired
     private BankAccountRepository bankAccountRepository;
 
     @Override
     public void saveAccount(BankAccount bankAccount) {
-        logger.debug("Saving bank account: {}", bankAccount);
+        log.debug("Saving bank account: {}", bankAccount);
+
         bankAccountRepository.save(bankAccount);
-        logger.debug("Saved bank account with id: {}", bankAccount.getId());
+
+        log.debug("Saved bank account with id: {}", bankAccount.getId());
     }
 
     @Override
     public BankAccount getAccount(UUID id) {
-        logger.debug("Fetching bank account with id: {}", id);
-        BankAccount bankAccount = null;
-        Optional<BankAccount> bankAccountOptional = bankAccountRepository.findById(id);
-        if (bankAccountOptional.isPresent()) {
-            bankAccount = bankAccountOptional.get();
-        }
+        log.debug("Fetching bank account with id: {}", id);
 
-        logger.debug("Found bank account: {}", bankAccount);
-        return bankAccount;
+        return bankAccountRepository.findById(id).orElseThrow(() -> new NoSuchBankAccountException("There is no bank account with such id"));
     }
 
     @Override
     public void deleteAccount(UUID id) {
-        logger.debug("Deleting bank account with id: {}", id);
+        bankAccountRepository.findById(id).orElseThrow(() -> new NoSuchBankAccountException("There is no bank account with such id"));
+
+        log.debug("Deleting bank account with id: {}", id);
+
         bankAccountRepository.deleteById(id);
-        logger.debug("Deleted bank account with id: {}", id);
+
+        log.debug("Deleted bank account with id: {}", id);
     }
 }
