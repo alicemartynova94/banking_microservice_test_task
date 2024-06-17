9fefd711901dc361384dd12cabb3bf22368f066e
Alice Martynova <alicemartynova94@gmail.com>
2024-06-17 14:48:05 +0300

    refactor: delete ligging for BankAccountController class

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
@@ -3,8 +3,6 @@ package com.banking.banking_microservice_test_task.controller;
 import com.banking.banking_microservice_test_task.entity.BankAccount;
 import com.banking.banking_microservice_test_task.exception_handling.NoSuchBankAccountException;
 import com.banking.banking_microservice_test_task.service.BankAccountService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,43 +11,35 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api")
 public class BankAccountController {
-    private static final Logger logger = LoggerFactory.getLogger(BankAccountController.class);
+
     @Autowired
     private BankAccountService bankAccountService;
 
     @GetMapping("/account/{id}")
     public BankAccount getAccount(@PathVariable UUID id) {
-        logger.info("Received request to get bank account with id: {}", id);
         BankAccount bankAccount = bankAccountService.getAccount(id);
 
         if (bankAccount == null) {
             throw new NoSuchBankAccountException("There is no bank account with id " + id + " in database.");
         }
 
-        logger.info("Responding with bank account details for id: {}", id);
         return bankAccount;
     }
 
     @PostMapping("/accounts")
     public BankAccount addNewAccount(@RequestBody BankAccount bankAccount) {
-        logger.info("Received request to save bank account: {}", bankAccount);
-
         bankAccountService.saveAccount(bankAccount);
-
-        logger.info("Bank account saved successfully with id: {}", bankAccount.getId());
         return bankAccount;
     }
 
     @DeleteMapping("/account/{id}")
     public void deleteAccount(@PathVariable UUID id) {
-        logger.info("Received request to delete bank account with id: {}", id);
-
         BankAccount bankAccount = bankAccountService.getAccount(id);
+
         if (bankAccount == null) {
             throw new NoSuchBankAccountException("There is no bank account with id " + id + " in database, so it cannot be deleted.");
         }
 
-        logger.info("Bank account with id: {} deleted successfully", id);
         bankAccountService.deleteAccount(id);
     }
 }
