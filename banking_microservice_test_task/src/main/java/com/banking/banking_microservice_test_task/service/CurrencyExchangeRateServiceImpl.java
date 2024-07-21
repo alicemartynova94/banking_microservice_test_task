13343c8d5d2735cb7a41e66c0420f532b9bb8836
Alice Martynova <alicemartynova94@gmail.com>
2024-07-21 20:13:18 +0300

    feat: move base url and api key values to properties file, build dynamic uri using UriComponents in getExchangeRateData method in service class

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
@@ -1,21 +1,30 @@
 package com.banking.banking_microservice_test_task.service;
 
 import com.banking.banking_microservice_test_task.entity.CurrencyExchangeRate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class CurrencyExchangeRateServiceImpl implements CurrencyExchangeRateService {
 
-    private static final String API_KEY = "e1677ba07b6c475fa2a42e7402131fe2";
-    private static final String BASE_URL = "https://api.twelvedata.com/exchange_rate";
+    @Value("${apikey}")
+    private String apiKey;
+    @Value("${baseurl}")
+    private String baseUrl;
 
     @Override
     public CurrencyExchangeRate getExchangeRateData(String currencyName) {
-        String resource_url = String.format("%s?symbol=%s&apikey=%s", BASE_URL, currencyName, API_KEY);
+        UriComponents builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("symbol", currencyName)
+                .queryParam("interval", "30min")
+                .queryParam("apikey", apiKey)
+                .build();
 
         RestTemplate restTemplate = new RestTemplate();
 
-        return restTemplate.getForObject(resource_url, CurrencyExchangeRate.class);
+        return restTemplate.getForObject(builder.toUriString(), CurrencyExchangeRate.class);
     }
 }
