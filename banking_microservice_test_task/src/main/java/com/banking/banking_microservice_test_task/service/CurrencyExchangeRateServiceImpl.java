7875d9fafeee300d94c5b30d3f55df74cd39c24f
Alice Martynova <alicemartynova94@gmail.com>
2024-07-30 14:50:04 +0300

    refactor: add RestTemplate as a dependency to the class

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.banking.banking_microservice_test_task.service;
 
 import com.banking.banking_microservice_test_task.entity.CurrencyExchangeRate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class CurrencyExchangeRateServiceImpl implements CurrencyExchangeRateService {
-
+    @Autowired
+    private RestTemplate restTemplate;
     @Value("${apikey}")
     private String apiKey;
     @Value("${baseurl}")
@@ -22,8 +24,6 @@ public class CurrencyExchangeRateServiceImpl implements CurrencyExchangeRateServ
                 .queryParam("interval", "30min")
                 .queryParam("apikey", apiKey)
                 .build();
-
-        RestTemplate restTemplate = new RestTemplate();
 
         return restTemplate.getForObject(builder.toUriString(), CurrencyExchangeRate.class);
     }
