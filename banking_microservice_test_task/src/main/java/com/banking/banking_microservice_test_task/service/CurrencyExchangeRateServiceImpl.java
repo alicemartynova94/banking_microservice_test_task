09b6f8a3dfcc0aeef84ba0ff3f420756e7cb8a4d
Alice Martynova <alicemartynova94@gmail.com>
2024-07-31 11:41:55 +0300

    feat: add CurrencyExchangeRateServiceProperties dependency and inject values for cron in Sheduled annotation, base url, interval and api key fields

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/service/CurrencyExchangeRateServiceImpl.java
@@ -1,6 +1,8 @@
 package com.banking.banking_microservice_test_task.service;
 
 import com.banking.banking_microservice_test_task.entity.CurrencyExchangeRate;
+import com.banking.banking_microservice_test_task.properties.CurrencyExchangeRateServiceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -11,17 +13,15 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class CurrencyExchangeRateServiceImpl implements CurrencyExchangeRateService {
 
-    @Value("${apikey}")
-    private String apiKey;
-    @Value("${baseurl}")
-    private String baseUrl;
+    @Autowired
+    private CurrencyExchangeRateServiceProperties properties;
 
     @Override
     public CurrencyExchangeRate getExchangeRateData(String currencyName) {
-        UriComponents builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+        UriComponents builder = UriComponentsBuilder.fromHttpUrl(properties.getBaseUrl())
                 .queryParam("symbol", currencyName)
-                .queryParam("interval", "30min")
-                .queryParam("apikey", apiKey)
+                .queryParam("interval", properties.getInterval())
+                .queryParam("apikey", properties.getApiKey())
                 .build();
 
         RestTemplate restTemplate = new RestTemplate();
@@ -29,7 +29,9 @@ public class CurrencyExchangeRateServiceImpl implements CurrencyExchangeRateServ
         return restTemplate.getForObject(builder.toUriString(), CurrencyExchangeRate.class);
     }
 
-    @Scheduled(cron = "00****")
+    @Scheduled(cron = "#{@currencyExchangeRateServiceProperties.cron}")
+
+    @Scheduled(cron = "#{@currencyExchangeRateServiceProperties.cron}")
     public void getExchangeRateDataScheduler(){
         getExchangeRateData("KZT/USD");
         getExchangeRateData("RUB/USD");
