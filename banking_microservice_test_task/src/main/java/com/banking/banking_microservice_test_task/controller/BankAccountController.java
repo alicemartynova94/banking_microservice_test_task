36faac92a57a99691c0c3d1151c47a230adc3419
Alice Martynova <alicemartynova94@gmail.com>
2024-07-31 21:07:10 +0300

    feat: add @ResponceStatus annotation with 201 status code

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/controller/BankAccountController.java
@@ -4,6 +4,7 @@ import com.banking.banking_microservice_test_task.dto.BankAccountDto;
 import com.banking.banking_microservice_test_task.entity.BankAccount;
 import com.banking.banking_microservice_test_task.service.BankAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,6 +22,7 @@ public class BankAccountController {
     }
 
     @PostMapping("/accounts")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public BankAccountDto addNewAccount(@RequestBody BankAccountDto bankAccountDto) {
         bankAccountService.saveAccount(bankAccountDto);
         return bankAccountDto;
