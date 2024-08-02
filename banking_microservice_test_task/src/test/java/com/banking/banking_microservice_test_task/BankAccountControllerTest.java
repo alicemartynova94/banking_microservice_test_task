e3cd9d759853c4b8c9efb641fffc08ffdeac58be
Alice Martynova <alicemartynova94@gmail.com>
2024-08-02 15:56:29 +0300

    refactor: switch off wildcard for mockito import

--- banking_microservice_test_task/src/test/java/com/banking/banking_microservice_test_task/BankAccountControllerTest.java
+++ banking_microservice_test_task/src/test/java/com/banking/banking_microservice_test_task/BankAccountControllerTest.java
@@ -14,9 +14,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
