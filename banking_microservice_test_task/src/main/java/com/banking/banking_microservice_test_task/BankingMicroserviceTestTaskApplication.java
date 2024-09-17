cffdf80fb531252340adb4d63b6e9962d1413975
Alice Martynova <alicemartynova94@gmail.com>
2024-09-17 15:40:34 +0300

    clean up code: remove unused imports from BankingMicroserviceTestTaskApplication

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplication.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/BankingMicroserviceTestTaskApplication.java
@@ -2,10 +2,7 @@ package com.banking.banking_microservice_test_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
