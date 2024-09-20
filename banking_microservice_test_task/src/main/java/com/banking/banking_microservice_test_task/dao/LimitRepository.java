b9357d02f1f9f2d36ab53e5f11da9c67c0171e54
Alice Martynova <alicemartynova94@gmail.com>
2024-09-20 11:07:04 +0300

    refactor: rename "Limit" class to "TransactionLimit"

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/dao/LimitRepository.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/dao/LimitRepository.java
@@ -1,9 +1,9 @@
 package com.banking.banking_microservice_test_task.dao;
 
-import com.banking.banking_microservice_test_task.entity.Limit;
+import com.banking.banking_microservice_test_task.entity.TransactionLimit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface LimitRepository extends JpaRepository<Limit, UUID> {
+public interface LimitRepository extends JpaRepository<TransactionLimit, UUID> {
 }
