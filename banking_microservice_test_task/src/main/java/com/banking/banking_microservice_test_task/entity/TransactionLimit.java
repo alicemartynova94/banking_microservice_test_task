90e696f9ee0356a18454ae5821c282636e1aba5d
Alice Martynova <alicemartynova94@gmail.com>
2024-10-03 21:06:38 +0300

    feat: add new fields: transactionCategory and limitCreationTime to the TransactionLimit class

--- banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/entity/TransactionLimit.java
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/entity/TransactionLimit.java
@@ -3,14 +3,15 @@ package com.banking.banking_microservice_test_task.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -25,6 +26,12 @@ public class TransactionLimit {
     private UUID id = UUID.randomUUID();
     @Column(name = "limit_sum")
     private Double limitSum;
+    @Column(name = "transaction_category")
+    @Enumerated(EnumType.ORDINAL)
+    private TransactionCategory transactionCategory;
+    @CreationTimestamp
+    @Column(name = "limit_creation_time")
+    private LocalDateTime limitCreationTime;
     @UpdateTimestamp
     @Column(name = "limit_last_update_time")
     private LocalDateTime limitLastUpdateTime;
