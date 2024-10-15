8b55b89beed51c4d0d66e7384b2035cd328a89f0
Alice Martynova <alicemartynova94@gmail.com>
2024-10-15 16:54:38 +0300

    feat: add TransactionDto with id, transactionSum, transactionCategory and currencyShortname fields

---
+++ banking_microservice_test_task/src/main/java/com/banking/banking_microservice_test_task/dto/TransactionDto.java
@@ -0,0 +1,21 @@
+package com.banking.banking_microservice_test_task.dto;
+
+import com.banking.banking_microservice_test_task.entity.TransactionCategory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDto {
+
+    private UUID id;
+    private Double transactionSum;
+    private TransactionCategory transactionCategory;
+    private CurrencyShortnameDto currencyShortname;
+}
