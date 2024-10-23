3cf3e9ecbf0365d8e59d19ef0e1d43c96cc41beb
Alice Martynova <alicemartynova94@gmail.com>
2024-10-23 17:42:03 +0300

    feat: add custom method findExceededTransactionsByBankAccountIdto TransactionRepository

--- banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/dao/TransactionRepository.java
+++ banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/dao/TransactionRepository.java
@@ -2,9 +2,13 @@ package com.banking.bankingmicroservicetask.dao;
 
 import com.banking.bankingmicroservicetask.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-
+    @Query(value = "SELECT * FROM transaction t WHERE t.bank_account_id = :accountId AND t.limit_exceeded = true", nativeQuery = true)
+    List<Transaction> findExceededLimitTransactionsByBankAccountId(@Param("accountId") UUID accountId);
 }
