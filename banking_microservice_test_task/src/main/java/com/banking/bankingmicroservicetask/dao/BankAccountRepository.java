8516b173f787a679aea340d33db8fe8ccb7f3393
Alice Martynova <alicemartynova94@gmail.com>
2024-10-28 11:41:33 +0300

    feat: add custom methods to find active BankAccount, Transaction and TransactionLimit classes

--- banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/dao/BankAccountRepository.java
+++ banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/dao/BankAccountRepository.java
@@ -2,8 +2,14 @@ package com.banking.bankingmicroservicetask.dao;
 
 import com.banking.bankingmicroservicetask.entity.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+
+    @Query("SELECT account FROM BankAccount account WHERE account.id = :id AND account.bankAccountDeletedTime IS NULL")
+    Optional<BankAccount> findByIdActiveAccount(@Param("id") UUID id);
 }
