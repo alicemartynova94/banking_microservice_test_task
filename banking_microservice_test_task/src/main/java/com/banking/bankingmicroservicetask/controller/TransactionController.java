65e4332b0eb2cb295598a27cf236dc55508e828b
Alice Martynova <alicemartynova94@gmail.com>
2024-10-29 12:58:50 +0300

    feat: add @Tag and @ApiResponses swagger annotations for methods in the TransactionController class

--- banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/controller/TransactionController.java
+++ banking_microservice_test_task/src/main/java/com/banking/bankingmicroservicetask/controller/TransactionController.java
@@ -3,6 +3,12 @@ package com.banking.bankingmicroservicetask.controller;
 import com.banking.bankingmicroservicetask.entity.Transaction;
 import com.banking.bankingmicroservicetask.service.TransactionService;
 import com.banking.bankingmicroservicetask.dto.TransactionDto;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,22 +27,41 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @Tag(name = "get", description = "Get methods of Transaction API.")
     @GetMapping("/transaction/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction created successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TransactionDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid transaction data"),
+            @ApiResponse(responseCode = "404", description = "Bank account for this transaction is not found")
+    })
     public TransactionDto getTransaction(@PathVariable UUID id) {
         return transactionService.getTransaction(id);
     }
 
+    @Tag(name = "get", description = "Get methods of Transaction API.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transactions that exceed limit or an empty list retrieved successfully",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Transaction.class))))
+    })
     @GetMapping("/transactions/{accountId}")
     public List<Transaction> getExceededLimitTransactions(@PathVariable UUID accountId) {
         return transactionService.getExceededLimitTransactions(accountId);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction created successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TransactionDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid transaction data")
+    })
     @PostMapping("/transactions")
     public TransactionDto addNewTransaction(@RequestBody TransactionDto transactionDto) {
         transactionService.saveTransaction(transactionDto);
         return transactionDto;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transaction deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Transaction not found")
+    })
     @DeleteMapping("/transaction/{id}")
     public void deleteTransaction(@PathVariable UUID id) {
         transactionService.deleteTransaction(id);
