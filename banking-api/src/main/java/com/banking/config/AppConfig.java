ffa899885506f0912ea2ddc72a7d784c3ee84d9b
Alice Martynova <alicemartynova94@gmail.com>
2024-12-06 13:55:36 +0300

    feat: add @EnableConfigurationProperties annotation to AppConfig class

--- banking-api/src/main/java/com/banking/config/AppConfig.java
+++ banking-api/src/main/java/com/banking/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.banking.config;
 
 import com.banking.properties.AppConfigProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
+@EnableConfigurationProperties(AppConfigProperties.class)
 @RequiredArgsConstructor
 public class AppConfig {
 
