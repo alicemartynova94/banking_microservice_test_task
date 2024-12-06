17e5cb9981216dbc86f962db814aa2e51c07fef9
Alice Martynova <alicemartynova94@gmail.com>
2024-12-06 14:21:43 +0300

    feat: add scanBasePackages attribute

--- front/src/main/java/org/example/AppFront.java
+++ front/src/main/java/org/example/AppFront.java
@@ -3,12 +3,9 @@ package org.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "com.banking")
+@SpringBootApplication(scanBasePackages = "com.banking")
 public class AppFront {
 
     public static final String SERVICE_NAME = "banking-front";
