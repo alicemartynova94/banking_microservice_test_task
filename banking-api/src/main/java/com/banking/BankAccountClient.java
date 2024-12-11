cda84259d3039efee765e40166a97804b8d4c609
Alice Martynova <alicemartynova94@gmail.com>
2024-12-11 11:44:53 +0300

    refactor: rewrite getAllAccounts method, use getForEntity instead of exchange

--- banking-api/src/main/java/com/banking/BankAccountClient.java
+++ banking-api/src/main/java/com/banking/BankAccountClient.java
@@ -3,13 +3,12 @@ package com.banking;
 import com.banking.api.BankAccountApi;
 import com.banking.dto.BankAccountDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,13 +25,12 @@ public class BankAccountClient implements BankAccountApi {
 
     @Override
     public List<BankAccountDto> getAllAccounts() {
-        ResponseEntity<List<BankAccountDto>> response = restTemplate.exchange(
+        ResponseEntity<BankAccountDto[]> response = restTemplate.getForEntity(
                 "/accounts",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<BankAccountDto>>() {}
+                BankAccountDto[].class
         );
-        return response.getBody();
+
+        return Arrays.asList(response.getBody());
     }
 
     @Override
